19ea273794b268e918d0ea7f2fd1740c5e8c3f3e
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2022-10-15 14:18:41 +0300

    #22 Missing TS feature bites again.

--- metadata/src/index.d.ts
+++ metadata/src/index.d.ts
@@ -2,7 +2,10 @@
 // If we just do "export * from", our index.[m]js file ends up with "export" statement as well, thus causing runtime errors.
 // Another option is rename .d.ts files into .ts files and end up with a bunch of empty .[m]js files and index.[m]js exporting those - not very optimal either.
 export type { HKTArg, Kind } from "./common";
-export type { MetadataProvider } from "./provider";
+export type {
+  MetadataProvider,
+  MetadataProviderForEndpoints,
+} from "./provider";
 export type {
   EndpointMetadataInformation,
   GetEndpointsMetadata,
