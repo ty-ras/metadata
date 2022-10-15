318b9f8de3b223c48a0735bcf852c23d4e1838e3
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2022-10-15 14:08:08 +0300

    #22 It is easier for consumers when provider type…
    … is split in two.

--- metadata/src/provider.d.ts
+++ metadata/src/provider.d.ts
@@ -2,7 +2,7 @@ import type * as data from "@ty-ras/data-backend";
 import type * as common from "./common";
 import type * as endpoint from "./endpoint";
 
-export interface MetadataProvider<
+export type MetadataProvider<
   TArgument extends common.HKTArg,
   TEndpointArg,
   TEndpointMD,
@@ -13,6 +13,31 @@ export interface MetadataProvider<
   TInputContents extends data.TInputContentsBase,
   TFinalMetadataArgs,
   TFinalMetadata,
+> = MetadataProviderForEndpoints<
+  TArgument,
+  TEndpointArg,
+  TEndpointMD,
+  TEndpointState,
+  TStringDecoder,
+  TStringEncoder,
+  TOutputContents,
+  TInputContents
+> & {
+  createFinalMetadata: (
+    args: TFinalMetadataArgs,
+    endpointsMetadatas: ReadonlyArray<TEndpointMD>,
+  ) => TFinalMetadata;
+};
+
+export interface MetadataProviderForEndpoints<
+  TArgument extends common.HKTArg,
+  TEndpointArg,
+  TEndpointMD,
+  TEndpointState,
+  TStringDecoder,
+  TStringEncoder,
+  TOutputContents extends data.TOutputContentsBase,
+  TInputContents extends data.TInputContentsBase,
 > {
   getEndpointsMetadata: endpoint.GetEndpointsMetadata<
     TArgument,
@@ -24,8 +49,4 @@ export interface MetadataProvider<
     TOutputContents,
     TInputContents
   >;
-  createFinalMetadata(
-    args: TFinalMetadataArgs,
-    endpointsMetadatas: ReadonlyArray<TEndpointMD>,
-  ): TFinalMetadata;
 }
